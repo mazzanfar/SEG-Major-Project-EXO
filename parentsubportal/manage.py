f8d4cc9c69a9da7d414560339b4943803d7e7d58
Maximilian Ghazanfar <maximilian.ghazanfar@kcl.ac.uk>
2020-02-14 11:51:45 +0000

    bug fixes

--- parentsubportal/manage.py
+++ parentsubportal/manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 
-def main():
+if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'parentsubportal.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -17,5 +17,4 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
-    main()
+
