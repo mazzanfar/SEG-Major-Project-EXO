c6738fddee58dfff98ab6b78435fdfd4b718a1f0
parsaEbi <parsa.ebrahimi@kcl.ac.uk>
2020-02-19 15:28:29 +0000

    fix an error

---
+++ parentsupportal/pages/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import News
+from .form import profile_Form
+
+# Create your views here.
+def welcome_page(request):
+    news = News.objects
+    return render(request, 'pages/welcome_page.html', {'news': news})
+
+def welcome_page2(request):
+    return render(request, 'pages/welcome_page2.html')
