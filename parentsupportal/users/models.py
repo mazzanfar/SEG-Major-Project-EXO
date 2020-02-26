83fc8898b2699b7ff1df9226fdc8d5cdd5c3eaee
Angelova, Dariya (k1888591) <dariya.angelova@kcl.ac.uk>
2020-02-26 11:22:06 +0000

    update

--- parentsupportal/users/models.py
+++ parentsupportal/users/models.py
@@ -7,6 +7,10 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
+    profession = models.CharField(max_length=200,default="default")
+    #gender = models.ChoiceField()
+    address = models.CharField(max_length=200,default="default")
+
     def __str__(self):
         return f'{self.user.username} Profile'
 
