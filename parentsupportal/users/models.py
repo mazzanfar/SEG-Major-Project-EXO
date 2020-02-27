989d09a73046f7d29b2a8a52c164936499d0a56d
Angelova, Dariya (k1888591) <dariya.angelova@kcl.ac.uk>
2020-02-27 16:20:15 +0000

    working

--- parentsupportal/users/models.py
+++ parentsupportal/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-#from PIL import Image
+from PIL import Image
 # Create your models here.
 
 
@@ -9,12 +9,12 @@ class Profile(models.Model):
     profession = models.CharField(max_length=200, default='default')
     address = models.CharField(max_length=200, default='default')
     gender = models.CharField(max_length=20, default='default')
-    #image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return self.user.username
     
-    """
+    
     def save_img(self, *args, **kwargs):
         super(Profile, self).save(*args, **kwargs)
 
@@ -24,6 +24,3 @@ class Profile(models.Model):
             output_size = (300,300)
             img.thumbnail(output_size)
             img.save(self.image.path)
-    """
-    
-
