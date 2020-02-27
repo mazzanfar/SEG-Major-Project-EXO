4f83886d1820315409396c7bbfe8286d5061198a
Dasha <k1888591@kcl.ac.uk>
2020-02-27 12:59:03 +0000

    fixed the profile bug

---
+++ users/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+#from PIL import Image
+# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profession = models.CharField(max_length=200, default='default')
+    address = models.CharField(max_length=200, default='default')
+    gender = models.CharField(max_length=20, default='default')
+    #image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return self.user.username
+    
+    """
+    def save_img(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+    """
+    
+
---
+++ users/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+from .forms import SignUpForm, ProfileForm
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+def register(request):
+    form = SignUpForm(request.POST)
+    profile_form = ProfileForm(request.POST)
+
+    if form.is_valid() and profile_form.is_valid():
+        userMan = form.save()
+        #Create a profile object without saving it yet
+        profile = profile_form.save(commit=False)
+
+        profile.user = userMan
+        profile.save()
+
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect('/')
+    else:
+        form = SignUpForm()
+        profile_form = ProfileForm()
+
+    return render(request, 'users/register.html', {'form': form, 'profile_form': profile_form})
+
+"""
+#This function is garbage and has no purpose/does not work -> note: Make it better
+@login_required
+def profile(request):
+    
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'users/profile.html', context)
+    """
