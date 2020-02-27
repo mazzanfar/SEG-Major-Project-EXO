c8e60ee5ed24f96973ea0b712bca711a00765663
Angelova, Dariya (k1888591) <dariya.angelova@kcl.ac.uk>
2020-02-27 16:12:44 +0000

    update

--- parentsupportal/users/models.py
+++ parentsupportal/users/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
+#from PIL import Image
+# Create your models here.
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-
-    profession = models.CharField(max_length=200,default="default")
-    #gender = models.ChoiceField()
-    address = models.CharField(max_length=200,default="default")
+    profession = models.CharField(max_length=200, default='default')
+    address = models.CharField(max_length=200, default='default')
+    gender = models.CharField(max_length=20, default='default')
+    #image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
-        return f'{self.user.username} Profile'
-
-    def save(self, *args, **kwargs):
+        return self.user.username
+    
+    """
+    def save_img(self, *args, **kwargs):
         super(Profile, self).save(*args, **kwargs)
 
         img = Image.open(self.image.path)
@@ -23,3 +24,6 @@ class Profile(models.Model):
             output_size = (300,300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+    """
+    
+
