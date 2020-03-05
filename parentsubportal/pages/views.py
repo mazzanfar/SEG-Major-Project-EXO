09ffec8e4d3a17590d973f80dff6ce005ca15560
parsaEbi <parsa.ebrahimi@kcl.ac.uk>
2020-03-05 17:44:19 +0000

    fixing the database

--- parentsubportal/pages/views.py
+++ parentsubportal/pages/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from .models import News
 
 # Create your views here.
 def welcome_page(request):
     news = News.objects
-    return render(request, 'pages/welcome_page.html', {'news': news})
+    return render(request, 'pages/welcome_page.html')
 
 def welcome_page2(request):
     return render(request, 'pages/welcome_page2.html')
