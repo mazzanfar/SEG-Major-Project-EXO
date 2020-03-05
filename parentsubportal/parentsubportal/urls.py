7c6ae65f370d2349933431dc121d6868d44473f7
parsaEbi <parsa.ebrahimi@kcl.ac.uk>
2020-03-05 16:15:45 +0000

    change it back

--- parentsubportal/parentsubportal/urls.py
+++ parentsubportal/parentsubportal/urls.py
@@ -56,10 +56,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 
-
-    path('register2/', user_views.register2, name='register2'),
-    path('home2/', user_views.home2, name='home2'),
-
 ]
 
 
