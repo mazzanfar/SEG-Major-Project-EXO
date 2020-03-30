2efe557078dfa67331f281015718feda94656d0f
“Hedi <hedigharbi@Hedis-MacBook-Pro.local>
2020-03-30 17:42:36 +0100

    Uploaded children files

--- parentsubportal/timeline/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Timeline
-# Register your models here.
-
-admin.site.register(Timeline)
--- parentsubportal/timeline/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Timeline
-
-class TimelineForm(forms.ModelForm):
-    class Meta:
-        model = Timeline
-        fields = ('header','age','pdf')
