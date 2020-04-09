d32dbac08c414c2409444083b0f70c0b9f87090b
Maximilian Ghazanfar <maximilian.ghazanfar@kcl.ac.uk>
2020-04-09 02:45:28 +0100

    fix view for crawler

--- parentsubportal/health_data/views.py
+++ parentsubportal/health_data/views.py
@@ -2,18 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import UrlForm
 from .data_fetch.crawler import scrape
+import pdb
 
 
 def healthData_page(request):
-    if request.method == 'GET':
-        form = UrlForm(request.GET)
-
+    
+    if request.method == 'POST':
+        pdb.set_trace()
+        form = UrlForm(request.POST)
         if form.is_valid():
-            print("called ")
-            scrape(UrlForm)
+            scrape(request.POST['url'])
             return HttpResponseRedirect('')
 
     else:
+        print("called 1")
         form = UrlForm()
 
     return render(request, 'health_data/healthData_page.html', {'form': form})
