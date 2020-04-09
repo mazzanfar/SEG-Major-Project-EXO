085fc541185eb0b6f96a7a2c37e97d2e8e88ad8e
Maximilian Ghazanfar <maximilian.ghazanfar@kcl.ac.uk>
2020-04-09 03:10:56 +0100

    fix

--- parentsubportal/health_data/views.py
+++ parentsubportal/health_data/views.py
@@ -2,13 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import UrlForm
 from .data_fetch.crawler import scrape
-import pdb
 
 
 def healthData_page(request):
     
     if request.method == 'POST':
-        pdb.set_trace()
         form = UrlForm(request.POST)
         if form.is_valid():
             scrape(request.POST['url'])
